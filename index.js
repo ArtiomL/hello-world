33f0194992ec9a8b28d16f0db1aafa52d5856ba3
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-12-11 00:51:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.header("Content-Type", "text/plain")
+	res.send('hello, world')
+})
+
+app.listen(8080, () => console.log('Node.js app listening on port 8080!'))
