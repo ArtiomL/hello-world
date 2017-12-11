ad66e24c29d92ee0c017a52ab79d5736bb49c84f
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-12-11 02:18:34 +0200

    Node.js app

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.header("Content-Type", "text/plain")
+	res.header('Content-Type', 'text/plain')
 	res.send('hello, world\n')
 })
+
+app.get('/kill', require('crasher'))
 
 app.listen(8080, () => console.log('Node.js app is listening on port: 8080'))
